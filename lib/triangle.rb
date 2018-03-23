486f52c932f77164ea3b11efefb82425bd342a24
Christine Cua <cloudycua@gmail.com>
2018-03-23 00:08:21 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,12 +9,14 @@ class Triangle
 
   def kind
     if s1 > 0 && s2 > 0 && s3 > 0
-      if s1 == s2 && s2 == s3
-        :equilateral
-      elsif s1 == s2 || s1 == s3 || s2 == s3
-        :isosceles
-      else
-        :scalene
+
+        if s1 == s2 && s2 == s3
+          :equilateral
+        elsif s1 == s2 || s1 == s3 || s2 == s3
+          :isosceles
+        else
+          :scalene
+        end
       end
     else
         raise TriangleError
